23fa26ad26131efae0e002b3bf27b12934dcb88a
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-09-29 22:28:34 -0500

    se integra proceso de enrutamiento basico para la api

--- api/v1/index.php
+++ api/v1/index.php
@@ -10,3 +10,30 @@ $request = $_SERVER['REQUEST_URI'];
 if (strpos($request, '/api/v1/users') !== false) {
     include_once './src/routes/UserRoute.php';
 }
+
+// Rutas para roles (rols)
+if (strpos($request, '/api/v1/rols') !== false) {
+    include_once './src/routes/RolesRoute.php';
+}
+
+// Rutas para estados (states)
+if (strpos($request, '/api/v1/states') !== false) {
+    include_once './src/routes/StatesRoute.php';
+}
+
+// Rutas para prioridades (priority)
+if (strpos($request, '/api/v1/priority') !== false) {
+    include_once './src/routes/PriorityRoute.php';
+}
+
+// Rutas para usuarios (tasks)
+if (strpos($request, '/api/v1/tasks') !== false) {
+    include_once './src/routes/TaskRoute.php';
+}
+
+// Rutas para usuarios (boards)
+/*
+if (strpos($request, '/api/v1/boards') !== false) {
+    include_once './src/routes/UserRoute.php';
+}
+*/
