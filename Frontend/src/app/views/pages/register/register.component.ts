4935b58ea9c62c5d6f0c0b670bdeb90b7f478fd5
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-10-30 16:03:40 -0500

    se integra login como ruta principal

--- Frontend/src/app/views/pages/register/register.component.ts
+++ Frontend/src/app/views/pages/register/register.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { IconDirective } from '@coreui/icons-angular';
 import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective } from '@coreui/angular';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-register',
-    templateUrl: './register.component.html',
-    styleUrls: ['./register.component.scss'],
-    standalone: true,
-    imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle]
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+  standalone: true,
+  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle]
 })
 export class RegisterComponent {
 
-  constructor() { }
+  constructor(public route: Router) { }
+
+  async submit(): Promise<void> {
+    this.route.navigate(["/dashboard"]);
+  }
 
 }
