225faa3adf61f818df0ef217b081cc127d17c7af
DiegoJGaviria <145846999+DiegoJGaviria@users.noreply.github.com>
2024-11-18 10:38:32 -0500

    se crea registro de las personas

--- Frontend/src/app/pages/auth/register/register.component.ts
+++ Frontend/src/app/pages/auth/register/register.component.ts
@@ -1,22 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { IconDirective } from '@coreui/icons-angular';
 import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective } from '@coreui/angular';
 import { Router } from '@angular/router';
+import { BaseComponent } from 'src/app/shared/core/base.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../shared/services/auth.service';
+import { UserI } from 'src/app/shared/interface/UserI.interface';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, HttpClientModule, ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle],
+  providers: [AuthService],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  standalone: true,
-  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle]
 })
-export class RegisterComponent {
+export class RegisterComponent extends BaseComponent implements OnInit {
 
-  constructor(public route: Router) { }
+  constructor(
+    public route: Router,
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) { super() }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+      idRol: [''],
+    });
+  }
 
   async submit(): Promise<void> {
-    this.route.navigate(["/dashboard"]);
+    try {
+      this.form.patchValue({ idRol: 2 });
+      if (this.form.valid) {
+        await this.authService.register(this.form.value);
+        this.handleSuccess('usuario creado correctamente!');
+        this.route.navigate(["/login"]);
+      } else {
+        this.handleError('campos no validos');
+      }
+    } catch (error: any) {
+      this.handleError('campos no validos');
+    }
   }
 
 }
